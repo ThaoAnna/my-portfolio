f17b29f9b1dffbcecf73bacf081ff3f2f42ef6b7
Thao Anna <annaphuongthao147@gmail.com>
2025-10-23 15:48:14 +0200

    update skill part

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import javaLogo from "../assets/javaLogo.png";
+import reactLogo from "../assets/reactLogo.png";
+import tailwindLogo from "../assets/tailwindLogo.png";
+import javascriptLogo from "../assets/javascriptLogo.png";
+
+
 
 const Skills: React.FC = () => {
   return (
@@ -8,12 +14,30 @@ const Skills: React.FC = () => {
         Here is the tech stack I have used
       </p>
       <div className="flex gap-4">
-        <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
+        <section>
+          <img className="py-2 px-2 w-40 h-40" src={reactLogo} alt="React logo" />
+          <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
           React
         </a>
-        <a className="bg-[#2C2F38] text-white font-medium px-6 py-3 rounded-full hover:bg-[#3A3E49] transition-colors">
+        </section>
+        <section>
+          <img className="py-2 px-2 w-40 h-40" src={javaLogo} alt="Java logo" />
+          <a className="bg-[#2C2F38] text-white font-medium px-8 py-2 rounded-full hover:bg-[#3A3E49] transition-colors">
           Java
         </a>
+        </section>
+        <section>
+          <img className="py-2 px-2 w-40 h-40" src={tailwindLogo} alt="Tailwind logo" />
+          <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
+          Tailwind
+        </a>
+        </section>
+        <section>
+          <img className="py-2 px-2 w-40 h-40" src={javascriptLogo} alt="JavaScript logo" />
+          <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
+          JavaScript
+        </a>
+        </section>
       </div>
     </section>
   );
