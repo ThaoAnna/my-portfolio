c38ee3839d8dc0a5e78e4c9fc535f349f4b85079
Thao Anna <annaphuongthao147@gmail.com>
2025-10-25 23:42:08 +0200

    update contact

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,6 @@ import tailwindLogo from "../assets/tailwindLogo.png";
 import javascriptLogo from "../assets/javascriptLogo.png";
 import htmlLogo from "../assets/htmlLogo.png";
 
-
 const Skills: React.FC = () => {
   return (
     <section id="skills" className="py-20 px-6 max-w-4xl mx-auto bg-gray-50">
@@ -17,32 +16,44 @@ const Skills: React.FC = () => {
         <section>
           <img className="py-5 px-5 w-40 h-40" src={htmlLogo} alt="HTML logo" />
           <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
-          HTML
-        </a>
+            HTML
+          </a>
         </section>
         <section>
-          <img className="py-5 px-5 w-40 h-40" src={tailwindLogo} alt="Tailwind logo" />
+          <img
+            className="py-5 px-5 w-40 h-40"
+            src={tailwindLogo}
+            alt="Tailwind logo"
+          />
           <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
-          Tailwind
-        </a>
+            Tailwind
+          </a>
         </section>
         <section>
-          <img className="py-5 px-5-2 w-40 h-40" src={javascriptLogo} alt="JavaScript logo" />
+          <img
+            className="py-5 px-5-2 w-40 h-40"
+            src={javascriptLogo}
+            alt="JavaScript logo"
+          />
           <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
-          JavaScript
-        </a>
+            JavaScript
+          </a>
         </section>
         <section>
-          <img className="py-5 px-5 w-40 h-40" src={reactLogo} alt="React logo" />
+          <img
+            className="py-5 px-5 w-40 h-40"
+            src={reactLogo}
+            alt="React logo"
+          />
           <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
-          React
-        </a>
+            React
+          </a>
         </section>
         <section>
           <img className="py-5 px-5 w-40 h-40" src={javaLogo} alt="Java logo" />
           <a className="bg-[#2C2F38] text-white font-medium px-8 py-2 rounded-full hover:bg-[#3A3E49] transition-colors">
-          Java
-        </a>
+            Java
+          </a>
         </section>
       </div>
     </section>
