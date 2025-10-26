b589f74805406a3919d66480bea9edff78be7084
Thao Anna <annaphuongthao147@gmail.com>
2025-10-26 16:39:00 +0100

    update skill part

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -12,46 +12,54 @@ const Skills: React.FC = () => {
       <p className="text-xl md:text-5xl font-semibold bg-gradient-to-r from-[#7CCBFF] to-[#B6A9FF] bg-clip-text text-transparent mb-4">
         Here is the tech stack I have used
       </p>
-      <div className="flex gap-4">
-        <section>
-          <img className="py-5 px-5 w-40 h-40" src={htmlLogo} alt="HTML logo" />
-          <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-12">
+        <section className=" flex flex-col justify-center items-center">
+          <img
+            className="w-28 h-28 mb-4 object-contain"
+            src={htmlLogo}
+            alt="HTML logo"
+          />
+          <a className="bg-[#68B5FF] text-white font-medium px-8 py-2 rounded-full hover:bg-[#4EA5F7] transition-colors">
             HTML
           </a>
         </section>
-        <section>
+        <section className="flex flex-col justify-center items-center">
           <img
-            className="py-5 px-5 w-40 h-40"
+            className="w-28 h-28 mb-4 object-contain"
             src={tailwindLogo}
             alt="Tailwind logo"
           />
-          <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
+          <a className="bg-[#68B5FF] text-white font-medium px-8 py-2 rounded-full hover:bg-[#4EA5F7] transition-colors">
             Tailwind
           </a>
         </section>
-        <section>
+        <section className="flex flex-col justify-center items-center">
           <img
-            className="py-5 px-5-2 w-40 h-40"
+            className="w-28 h-28 mb-4 object-contain"
             src={javascriptLogo}
             alt="JavaScript logo"
           />
-          <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
+          <a className="bg-[#68B5FF] text-white font-medium px-8 py-2 rounded-full hover:bg-[#4EA5F7] transition-colors">
             JavaScript
           </a>
         </section>
-        <section>
+        <section className="flex flex-col justify-center items-center">
           <img
-            className="py-5 px-5 w-40 h-40"
+            className="w-28 h-28 mb-4 object-contain"
             src={reactLogo}
             alt="React logo"
           />
-          <a className="bg-[#68B5FF] text-white font-medium px-6 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors">
+          <a className="bg-[#68B5FF] text-white font-medium px-8 py-2 rounded-full hover:bg-[#4EA5F7] transition-colors">
             React
           </a>
         </section>
-        <section>
-          <img className="py-5 px-5 w-40 h-40" src={javaLogo} alt="Java logo" />
-          <a className="bg-[#2C2F38] text-white font-medium px-8 py-2 rounded-full hover:bg-[#3A3E49] transition-colors">
+        <section className="flex flex-col justify-center items-center">
+          <img
+            className="w-28 h-28 mb-4 object-contain"
+            src={javaLogo}
+            alt="Java logo"
+          />
+          <a className="bg-[#68B5FF] text-white font-medium px-8 py-2 rounded-full hover:bg-[#4EA5F7] transition-colors">
             Java
           </a>
         </section>
