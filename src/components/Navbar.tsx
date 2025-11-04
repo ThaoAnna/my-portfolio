9428475575ae2976399bdecf40f10d012a69c37e
Thao Anna <annaphuongthao147@gmail.com>
2025-11-05 00:13:52 +0100

    update the color of header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ const Navbar: React.FC = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-10 border-b border-black/5 dark:border-white/10">
       <div className="bg-white dark:bg-gray-950">
-        <div className="flex h-16 items-center justify-between gap-8 px-4 sm:pz-6">
-          <div className="flex flex-row w-full justify-between">
-            <h1 className="text-xl font-bold">Anna Thao</h1>
+        <div className="flex h-15 items-center justify-between gap-8 px-4 sm:pz-6">
+          <div className="flex flex-row w-full justify-between text-sd-blue-500 bg-clip-text">
+            <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500">Anna Thao</h1>
           </div>
           <div className="flex items-center gap-6 max-md:hidden">
               <button className="inline-flex items-center gap-1 rounded-full bg-gray-950/2 px-2 py-1">
