d78f1e5b118b5a768c8f27c3f49d42c36da00aad
Thao Anna <annaphuongthao147@gmail.com>
2025-11-07 21:17:51 +0100

    change width and height in footer

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,21 +5,21 @@ import linkedinLogo from "../assets/linkedinLogo.png";
 
 const Contact: React.FC = () => {
   return (
-    <section id="contact" className="py-20 px-6 max-w-4xl mx-auto text-center">
+    <section id="contact" className="py-10 mt-15 max-w-5xl mx-auto text-center">
       <div>
-        <h2 className="text-3xl md:text-4xl font-bold mb-10">
+        <h2 className="text-xl md:text-4xl font-semibold bg-gradient-to-r from-[#7CCBFF] to-[#B6A9FF] bg-clip-text text-transparent mb-4 pb-8">
           If your're interested to work with me, just drop me in{" "}
         </h2>
       </div>
       <div className="flex flex-col md:flex-row justify-center items-center gap-12">
         <section className="flex flex-col justify-center items-center">
           <img
-            className="w-28 h-28 mb-4 object-contain"
+            className="w-18 h-18 mb-4 object-contain"
             src={githubLogo}
             alt="Github logo"
           />
           <a 
-          className="bg-[#68B5FF] text-white font-medium px-8 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors"
+          className="bg-[#68B5FF] text-white font-medium px-6 py-1 rounded-full hover:bg-[#4EA5F7] transition-colors"
           target="_blank"
           href="https://github.com/ThaoAnna"
           rel="noopener noreferrer">
@@ -28,12 +28,12 @@ const Contact: React.FC = () => {
         </section>
         <section className="flex flex-col justify-center items-center" >
           <img
-            className="py-5 px-5 w-30 h-30 justify-center "
+            className="w-18 h-18 mb-4 object-contain "
             src={gmailLogo}
             alt="Gmail logo"
           />
           <a 
-          className="bg-[#68B5FF] text-white font-medium px-8 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors"
+          className="bg-[#68B5FF] text-white font-medium px-6 py-1 rounded-full hover:bg-[#4EA5F7] transition-colors"
           target="_blank"
           href="https://github.com/ThaoAnna"
           rel="noopener noreferrer">
@@ -42,12 +42,12 @@ const Contact: React.FC = () => {
         </section>
         <section className="flex flex-col justify-center items-center">
           <img
-            className="py-5 px-5 w-30 h-30"
+            className="w-18 h-18 mb-4 object-contain"
             src={linkedinLogo}
             alt="LinkedIn logo"
           />
           <a 
-          className="bg-[#68B5FF] text-white font-medium px-8 py-3 rounded-full hover:bg-[#4EA5F7] transition-colors"
+          className="bg-[#68B5FF] text-white font-medium px-6 py-1 rounded-full hover:bg-[#4EA5F7] transition-colors"
           target="_blank"
           href="https://www.linkedin.com/feed/"
           rel="noopener noreferrer">
